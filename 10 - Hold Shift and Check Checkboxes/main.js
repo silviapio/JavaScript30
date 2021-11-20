b49ee66c4d3b51fc1bfc78214b332253e4491ecc
Silvia <silvia.piovesan@gmail.com>
2021-11-20 12:34:08 +0100

    p10-refactored and simplified

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -1,68 +1,26 @@
 const checkInputs = document.querySelectorAll("input");
+let lastChecked;
 
-console.log(checkInputs);
+checkInputs.forEach(input => {
+    input.addEventListener("click", handleCheckClick)
+});
 
-const firstCheckBoxClicked = "";
-const secondCheckBoxClicked = "";
-
-let checkedInputsIds = [];
-
-checkInputs.forEach(checkInput => {
-    checkInput.addEventListener("click", (e) => {
-        const clickedCheckBox = e.target;
-        if (clickedCheckBox.checked) {
-            console.log(`checkbox ${clickedCheckBox.dataset.id} clicked`);
-            // check if at least one other checkbox was checked
-            const lastCheckedId = getLastChecked();
-            console.log(`last checked id: ${lastCheckedId}`)
-            if (checkedInputsIds) {
-                if (e.shiftKey) {
-                    console.log(`will select from lastchecked (${lastCheckedId}) to current (${clickedCheckBox.dataset.id})`)
-                    selectMultipleCheckBoxes(lastCheckedId, parseInt(clickedCheckBox.dataset.id));
-                }
-            }
-            // check if there is a lower id checkbox
-            // 
-            checkedInputsIds.push(parseInt(clickedCheckBox.dataset.id));
-        } else {
-            checkedInputsIds = [...checkedInputsIds].filter(input => input !== parseInt(clickedCheckBox.dataset.id));
-        }
-        console.log(checkedInputsIds);
+function handleCheckClick(e) {
+    const isMultipleSelection = this.checked && e.shiftKey;
+    if (isMultipleSelection) {
+        checkInputsInBetween(this);
     }
+    lastChecked = this;
+}
 
-    )
-})
-
-function getLastChecked() {
-    return checkedInputsIds[checkedInputsIds.length-1];
-} 
-
-
-function selectMultipleCheckBoxes(lastChecked, currentChecked) {
-    checkInputs.forEach(checkInput => {
-        const checkInputId = parseInt(checkInput.dataset.id);
-        if (isSelectionDownwards(lastChecked, currentChecked)) {
-            if (checkInputId > lastChecked && checkInputId < currentChecked) {
-                checkInput.setAttribute("checked", "");
-                addCheckedIdToArray(checkInputId);
-                console.log(checkInput);
-            }
-        } else {
-            if (checkInputId < lastChecked && checkInputId > currentChecked) {
-                checkInput.setAttribute("checked", "");
-                addCheckedIdToArray(checkInputId);
-                console.log(checkInput);
-            }
+function checkInputsInBetween(currentChecked) {
+    let inBetween = false;
+    checkInputs.forEach(input => {
+        if (input === currentChecked || input === lastChecked) {
+            inBetween = !inBetween;
+        }
+        if (inBetween) {
+            input.checked = true;
         }
     })
 }
-
-function isSelectionDownwards(lastChecked, currentChecked) {
-    return currentChecked > lastChecked;
-}
-
-function addCheckedIdToArray(id) {
-    checkedInputsIds.push(id);
-}
-
-
